2bc9f9370d609bab881d3dae454dc5f43631e28c
Nurwaely12 <nurwaely@gmail.com>
2024-10-02 15:04:48 +0700

    Program latihan113

---
+++ modifikasipemilihan13.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class modifikasipemilihan13 {
+    public static void main(String[] args) {
+        Scanner input13 = new Scanner(System.in);
+        String kategori;
+        int penghasilan, gajiBersih;
+        double pajak = 0;
+
+        System.out.print("Masukkan kategori : ");
+        kategori = input13.nextLine();
+        System.out.print("Masukkan Besarnya Penghasilan : ");
+        penghasilan = input13.nextInt();
+
+        if (kategori.equals("pekerja")) {
+            if (penghasilan <= 20000000)
+            pajak = 0.1;
+            else if (penghasilan <= 30000000)
+            pajak = 0.15;
+            else
+            pajak = 0.2;
+            gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.print("Penghasilan Bersih : " + gajiBersih);
+        }else if (kategori.equals("pebisnis")) {
+            if (penghasilan <= 25000000)
+            pajak = 0.15;
+            else if (penghasilan <= 35000000)
+            pajak = 0.2;
+            else
+            pajak = 0.25;
+            gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.print("penghasil Bersih : " + gajiBersih);
+        } else
+            System.out.println("Masukkan Kategori salah");
+    }
+    
+}
